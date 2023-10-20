49b38bab4a68d1b1e290f41dcd6cff3cbf44a924
André Pedersen <andrped94@gmail.com>
2023-10-20 23:11:21 +0200

    Added bug fixes to the demo

--- demo/src/compute.py
+++ demo/src/compute.py
@@ -1,6 +1,6 @@
 import subprocess as sp
 
 
-def run_model(fixed_path, moving_path, output_path):
+def run_model(fixed_path, moving_path, output_path, task):
     sp.check_call(["ddmr", "--fixed", fixed_path, "--moving", moving_path, \
-                   "-o", output_path, "-a", "B", "--model", "BL-NS", "--original-resolution"])
+                   "-o", output_path, "-a", task, "--model", "BL-NS", "--original-resolution"])
