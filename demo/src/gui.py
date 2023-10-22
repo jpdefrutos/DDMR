d24d99414780cf3d62458b929e8e4ddd8dc8e66e
André Pedersen <andrped94@gmail.com>
2023-10-22 13:12:25 +0200

    Refactor

--- demo/src/gui.py
+++ demo/src/gui.py
@@ -84,6 +84,7 @@ class WebUI:
         self.fixed_images = load_ct_to_numpy(self.fixed_image_path)
         self.moving_images = load_ct_to_numpy(self.moving_image_path)
         self.pred_images = load_ct_to_numpy(output_path + "pred_image.nii.gz")
+
         return None
 
     def get_fixed_image(self, k):
